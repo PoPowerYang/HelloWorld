5e49d076fc3d19bf285c36b775dc2fc4e6796116
PoPowerYang <yangsunsunshine@gmail.com>
2023-09-18 21:53:50 -0400

    use for loop to print hello world

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -2,9 +2,19 @@
 // Description: a program that prints out hello world
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
-int main() {
-	cout << "Hello World" << endl;
+int main() 
+{
+	cout << std::setiosflags(ios::left);
+	for (int i = 0; i < 6; i++) 
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			cout << setw(17) << "Hello World";
+		}
+		cout << endl;
+	}
 	return 0;
 }
